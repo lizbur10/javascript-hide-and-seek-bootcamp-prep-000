c2ac23487c25e0fc910e9b390cb1383399cf2133
Liz Burton <liz.burton147@gmail.com>
2017-10-31 20:06:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('.ranked-list li');
+    for (let i=0; i<rankedList.length; i++) {
+      rankedList[i].innerHTML = (parseInt(rankedList[i].innerHTML) + parseInt(n)).toString();
+    }
+  }
+
+
+function deepestChild() {
+  var temp;
+  var i=0;
+  var current = document.querySelector('#grand-node');
+    while (current.children.length > 0) {
+    current = current.firstElementChild;
+  }
+  return current;
+
+//    while (current); {
+//      console.log('running');
+//      current = current.children;
+//    }
+//    return current;
+//  console.log(current.innerHTML);
+}
